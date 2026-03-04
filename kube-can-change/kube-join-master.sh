0ea9c63715fc39e6c23048a3d65361d61a7c4a2a
jinlinsong <cppcan@163.com>
2026-03-04 15:24:44 +0800

    普通join

---
+++ kube-can-change/kube-join-master.sh
@@ -0,0 +1,4 @@
+kubeadm join 192.168.1.250:6443 --token th215m.36fvgzfv424dsuw9 \
+        --discovery-token-ca-cert-hash sha256:b2ab702211bdc47f1416562cf20676f584fa044df9972fc504482c29d2645930 \
+        --control-plane --certificate-key 78fdd303a3cfe1c0b196223969dc9a614ec24e403ff61747bb3ce7bf5df31146
+
